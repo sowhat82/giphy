97ded7a73e79f16205d4ef25030846671ea6a8a1
Alvin Cheong <alvincheong@u.nus.edu>
2020-11-04 12:33:16 +0800

    new2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 const withquery = require('with-query').default
 
 //configure the PORT
-const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT)
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
 const apikey = process.env.apikey || ""
 const giphyurl = 'https://api.giphy.com/v1/gifs/search'
 
